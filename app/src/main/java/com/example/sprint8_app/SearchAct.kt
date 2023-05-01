ced09621431ac6105d82b71912009d4d0b683372
name <name@name.na>
2023-05-01 17:55:59 +0900

    Промежуточное сохранение (нет сохранения EditText)

--- app/src/main/java/com/example/sprint8_app/SearchAct.kt
+++ app/src/main/java/com/example/sprint8_app/SearchAct.kt
@@ -1,11 +1,60 @@
 package com.example.sprint8_app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
 
 class SearchAct : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+        val inputSearch = findViewById<EditText>(R.id.searchView)
+        val layoutSearch = findViewById<LinearLayout>(R.id.linearLayout)
+        val resetSearch = findViewById<ImageView>(R.id.clear_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        settingsButton.setOnClickListener {
+            val displayIntent = Intent(Intent(this, SettingsActivity::class.java))
+            startActivity(displayIntent)
+        }
+
+        resetSearch.setOnClickListener {
+            inputSearch.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // ждите ответа в следующей серии
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.isNullOrEmpty()) {
+                    // ждите ответа в следующей серии
+                } else {
+                    // ждите ответа в следующей серии
+                }
+                resetSearch.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // ждите ответа в следующей серии
+            }
+        }
+        inputSearch.addTextChangedListener(simpleTextWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
--- app/src/main/java/com/example/sprint8_app/SettingsActivity.kt
+++ app/src/main/java/com/example/sprint8_app/SettingsActivity.kt
@@ -1,14 +1,54 @@
 package com.example.sprint8_app
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
         val backButton = findViewById<ImageView>(R.id.back_button)
+        val shareButton: TextView = findViewById(R.id.share_button)
+        val supportButton: TextView = findViewById(R.id.support_button)
+        val userAgreementButton: TextView = findViewById(R.id.user_agreement_button)
+        val searchButton: Button = findViewById(R.id.buttonSearch)
+
+        searchButton.setOnClickListener {
+            val displayIntent = Intent(Intent(this, SearchAct::class.java))
+            startActivity(displayIntent)
+        }
+
+        shareButton.setOnClickListener {
+            val practicumWeb = "https://practicum.yandex.ru/android-developer/"
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.setType("text/plain")
+            shareIntent.putExtra(Intent.EXTRA_TEXT, practicumWeb)
+            startActivity(Intent.createChooser(shareIntent, "Шарим нашу приложуху"))
+        }
+
+        supportButton.setOnClickListener {
+            val email = "madmaks.45cal@gmail.com"
+            val tema = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val tekst = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val shareIntent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:").buildUpon().appendQueryParameter("to", email)
+                    .appendQueryParameter("subject", tema).appendQueryParameter("body", tekst).build()
+            }
+            startActivity(shareIntent)
+        }
+
+        userAgreementButton.setOnClickListener {
+            val practicumWeb = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val agreeIntent = Intent(Intent.ACTION_VIEW, practicumWeb)
+            startActivity(agreeIntent)
+        }
 
         backButton.setOnClickListener {
             finish()
